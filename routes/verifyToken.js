7e6943ad51999b25b7e28964b35d752f74954288
Babar kk <mabkiani36@gmail.com>
2021-07-01 13:09:32 +0500

    nodejs-mongodb

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,11 +1,15 @@
 const jwt = require("jsonwebtoken");
 const token_secret = require("../config/custom-environment-variables.json");
+const verify = require("../routes/verifyToken");
 module.exports = (req, res, next) => {
     const token = req.header("auth-token");
     if (!token) return res.status(400).send("Access Denied");
 
     try {
         const verified = jwt.verify(token, token_secret.jwtPrivateKey);
+
+        req.user = verified;
+        next();
     } catch (error) {
         res.status(400).send("Invalid Token");
     }
